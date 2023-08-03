38f4a557d0990cc62baace08133fcec94a83ecbc
guiseninha <107883094+guiseninha@users.noreply.github.com>
2023-08-03 16:12:00 -0300

    Add files via upload

---
+++ 111--main/114.py
@@ -0,0 +1,15 @@
+a = 33
+b = 33
+if b > a: print("b é maior que a")
+elif b == a: print("a e b são iguais")
+if b == a:
+    print("b é maior que a")
+elif b == a:
+    print("a e b são iguais")
+
+def my_fuction(fname):
+    print(fname + "Refsnes")
+
+    my_fuction("Emilly")
+    my_fuction("Tobias")
+    my_fuction("Lucas")
